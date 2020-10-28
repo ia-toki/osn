e5dd070287e563a995c0e4bc70aa22ecd9e753af
Ashar Fuadi <fushar@gmail.com>
2020-10-28 22:55:36 +0700

    Add participants medal count

--- app/Controllers/BaseController.php
+++ app/Controllers/BaseController.php
@@ -39,4 +39,137 @@ class BaseController extends Controller
 		$this->db = \Config\Database::connect();
 	}
 
+	protected function getProvinceMedals() {
+		return $this->db->query(<<<QUERY
+			select p.ID as ID, pr.Name as Name, Golds, Silvers, Bronzes, Participants from (
+				select distinct(Province) as ID from Contestant
+			) as p
+			join Province pr on p.ID = pr.ID
+			left join (
+				select Province as ID, count(Medal) as Golds
+				from Contestant
+				where Medal = 'G'
+				group by Province
+			) as golds on p.ID = golds.ID
+			left join (
+				select Province as ID, count(Medal) as Silvers
+				from Contestant
+				where Medal = 'S'
+				group by Province
+			) as silvers on p.ID = silvers.ID
+			left join (
+				select Province as ID, count(Medal) as Bronzes
+				from Contestant
+				where Medal = 'B'
+				group by Province
+			) as bronzes on p.ID = bronzes.ID
+			left join (
+				select Province as ID, count(Medal) as Participants
+				from Contestant
+				where Medal = ''
+				group by Province
+			) as participants on p.ID = participants.ID
+			order by coalesce(Golds, 0) desc, coalesce(Silvers, 0) desc, coalesce(Bronzes, 0) desc, coalesce(Participants, 0), Name asc
+		QUERY)->getResultArray();
+	}
+
+	protected function getPersonMedals($id) {
+		return $this->db->query(sprintf(<<<QUERY
+			select c.ID as ID, Name,
+			InternationalGolds, InternationalSilvers, InternationalBronzes, InternationalParticipants,
+			RegionalGolds, RegionalSilvers, RegionalBronzes, RegionalParticipants,
+			NationalGolds, NationalSilvers, NationalBronzes, NationalParticipants
+			from (
+				select ID, Name from Person %s
+			) as c
+			left join (
+				select Person, count(Medal) as InternationalGolds
+				from Contestant c
+				join Competition comp on comp.ID = c.Competition
+				where comp.Level = 'International' and Medal = 'G'
+				group by Person
+			) as iGolds on c.ID = iGolds.Person
+			left join (
+				select Person, count(Medal) as InternationalSilvers
+				from Contestant c
+				join Competition comp on comp.ID = c.Competition
+				where comp.Level = 'International' and Medal = 'S'
+				group by Person
+			) as iSilvers on c.ID = iSilvers.Person
+			left join (
+				select Person, count(Medal) as InternationalBronzes
+				from Contestant c
+				join Competition comp on comp.ID = c.Competition
+				where comp.Level = 'International' and Medal = 'B'
+				group by Person
+			) as iBronzes on c.ID = iBronzes.Person
+			left join (
+				select Person, count(Medal) as InternationalParticipants
+				from Contestant c
+				join Competition comp on comp.ID = c.Competition
+				where comp.Level = 'International' and Medal = ''
+				group by Person
+			) as iParticipants on c.ID = iParticipants.Person
+			left join (
+				select Person, count(Medal) as RegionalGolds
+				from Contestant c
+				join Competition comp on comp.ID = c.Competition
+				where comp.Level = 'Regional' and Medal = 'G'
+				group by Person
+			) as rGolds on c.ID = rGolds.Person
+			left join (
+				select Person, count(Medal) as RegionalSilvers
+				from Contestant c
+				join Competition comp on comp.ID = c.Competition
+				where comp.Level = 'Regional' and Medal = 'S'
+				group by Person
+			) as rSilvers on c.ID = rSilvers.Person
+			left join (
+				select Person, count(Medal) as RegionalBronzes
+				from Contestant c
+				join Competition comp on comp.ID = c.Competition
+				where comp.Level = 'Regional' and Medal = 'B'
+				group by Person
+			) as rBronzes on c.ID = rBronzes.Person
+			left join (
+				select Person, count(Medal) as RegionalParticipants
+				from Contestant c
+				join Competition comp on comp.ID = c.Competition
+				where comp.Level = 'Regional' and Medal = ''
+				group by Person
+			) as rParticipants on c.ID = rParticipants.Person
+			left join (
+				select Person, count(Medal) as NationalGolds
+				from Contestant c
+				join Competition comp on comp.ID = c.Competition
+				where comp.Level = 'National' and Medal = 'G'
+				group by Person
+			) as nGolds on c.ID = nGolds.Person
+			left join (
+				select Person, count(Medal) as NationalSilvers
+				from Contestant c
+				join Competition comp on comp.ID = c.Competition
+				where comp.Level = 'National' and Medal = 'S'
+				group by Person
+			) as nSilvers on c.ID = nSilvers.Person
+			left join (
+				select Person, count(Medal) as NationalBronzes
+				from Contestant c
+				join Competition comp on comp.ID = c.Competition
+				where comp.Level = 'National' and Medal = 'B'
+				group by Person
+			) as nBronzes on c.ID = nBronzes.Person
+			left join (
+				select Person, count(Medal) as NationalParticipants
+				from Contestant c
+				join Competition comp on comp.ID = c.Competition
+				where comp.Level = 'National' and Medal = ''
+				group by Person
+			) as nParticipants on c.ID = nParticipants.Person
+			order by coalesce(InternationalGolds, 0) desc, coalesce(InternationalSilvers, 0) desc, coalesce(InternationalBronzes, 0) desc, coalesce(InternationalParticipants, 0) desc,
+			coalesce(RegionalGolds, 0) desc, coalesce(RegionalSilvers, 0) desc, coalesce(RegionalBronzes, 0) desc, coalesce(RegionalParticipants, 0) desc,
+			coalesce(NationalGolds, 0) desc, coalesce(NationalSilvers, 0) desc, coalesce(NationalBronzes, 0) desc, coalesce(NationalParticipants, 0) desc, Name asc
+			limit 100
+		QUERY, $id ? 'where ID = ?' : ''), [$id])->getResultArray();
+	}
 }
--- app/Controllers/Statistics.php
+++ app/Controllers/Statistics.php
@@ -5,31 +5,7 @@ class Statistics extends BaseController {
 		helper('medal');
 		helper('link');
 
-		$provinces = $this->db->query(<<<QUERY
-			select p.ID as ID, pr.Name as Name, coalesce(Golds, 0) as Golds, coalesce(Silvers, 0) as Silvers, coalesce(Bronzes, 0) as Bronzes, coalesce(Golds, 0) + coalesce(Silvers, 0) + coalesce(Bronzes, 0) as Medals from (
-				select distinct(Province) as ID from Contestant
-			) as p
-			join Province pr on p.ID = pr.ID
-			left join (
-				select Province as ID, count(Medal) as Golds
-				from Contestant
-				where Medal = 'G'
-				group by Province
-			) as golds on p.ID = golds.ID
-			left join (
-				select Province as ID, count(Medal) as Silvers
-				from Contestant
-				where Medal = 'S'
-				group by Province
-			) as silvers on p.ID = silvers.ID
-			left join (
-				select Province as ID, count(Medal) as Bronzes
-				from Contestant
-				where Medal = 'B'
-				group by Province
-			) as bronzes on p.ID = bronzes.ID
-			order by Golds desc, Silvers desc, Bronzes desc, Name asc
-		QUERY)->getResultArray();
+		$medals = $this->getProvinceMedals();
 
 		$table = new \CodeIgniter\View\Table();
 		$table->setTemplate([
@@ -38,19 +14,16 @@ class Statistics extends BaseController {
 
 		$table->setHeading(
 			'Provinsi',
-			['data' => 'Emas', 'class' => 'col-centered'],
-			['data' => 'Perak', 'class' => 'col-centered'],
-			['data' => 'Perunggu', 'class' => 'col-centered'],
-			['data' => 'Total', 'class' => 'col-centered']
+			['data' => 'Nasional', 'class' => 'col-centered', 'colspan' => 4]
 		);
 
-		foreach ($provinces as $p) {
+		foreach ($medals as $m) {
 			$table->addRow(
-				linkProvince($p['ID'], $p['Name']),
-				['data' => $p['Golds'], 'class' => 'col-medals ' . getMedalClass('G')],
-				['data' => $p['Silvers'], 'class' => 'col-medals ' . getMedalClass('S')],
-				['data' => $p['Bronzes'], 'class' => 'col-medals ' . getMedalClass('B')],
-				['data' => $p['Medals'], 'class' => 'col-medals'],
+				linkProvince($m['ID'], $m['Name']),
+				['data' => $m['Golds'], 'class' => 'col-medals ' . getMedalClass('G')],
+				['data' => $m['Silvers'], 'class' => 'col-medals ' . getMedalClass('S')],
+				['data' => $m['Bronzes'], 'class' => 'col-medals ' . getMedalClass('B')],
+				['data' => $m['Participants'], 'class' => 'col-medals'],
 			);
 		}
 
@@ -184,7 +157,7 @@ class Statistics extends BaseController {
 		helper('medal');
 		helper('link');
 
-		$medals = $this->getMedalStatistics(null);
+		$medals = $this->getPersonMedals(null);
 
 		$table = new \CodeIgniter\View\Table();
 		$table->setTemplate([
@@ -193,24 +166,27 @@ class Statistics extends BaseController {
 
 		$heading = array(
 			'Nama',
-			['data' => 'Internasional', 'class' => 'col-centered', 'colspan' => '3'],
-			['data' => 'Regional', 'class' => 'col-centered', 'colspan' => '3'],
-			['data' => 'Nasional', 'class' => 'col-centered', 'colspan' => '3']
+			['data' => 'Internasional', 'class' => 'col-centered', 'colspan' => '4'],
+			['data' => 'Regional', 'class' => 'col-centered', 'colspan' => '4'],
+			['data' => 'Nasional', 'class' => 'col-centered', 'colspan' => '4']
 		);
 		$table->setHeading($heading);
 
 		foreach ($medals as $m) {
 			$table->addRow(
 				linkPerson($m['ID'], $m['Name']),
-				['data' => $m['InternationalGolds'] ?? '-', 'class' => 'col-statistics-person-medal medal--gold'],
-				['data' => $m['InternationalSilvers'] ?? '-', 'class' => 'col-statistics-person-medal medal--silver'],
-				['data' => $m['InternationalBronzes'] ?? '-', 'class' => 'col-statistics-person-medal medal--bronze'],
-				['data' => $m['RegionalGolds'] ?? '-', 'class' => 'col-statistics-person-medal medal--gold'],
-				['data' => $m['RegionalSilvers'] ?? '-', 'class' => 'col-statistics-person-medal medal--silver'],
-				['data' => $m['RegionalBronzes'] ?? '-', 'class' => 'col-statistics-person-medal medal--bronze'],
-				['data' => $m['NationalGolds'] ?? '-', 'class' => 'col-statistics-person-medal medal--gold'],
-				['data' => $m['NationalSilvers'] ?? '-', 'class' => 'col-statistics-person-medal medal--silver'],
-				['data' => $m['NationalBronzes'] ?? '-', 'class' => 'col-statistics-person-medal medal--bronze'],
+				['data' => $m['InternationalGolds'] ?? '-', 'class' => 'col-medals medal--gold'],
+				['data' => $m['InternationalSilvers'] ?? '-', 'class' => 'col-medals medal--silver'],
+				['data' => $m['InternationalBronzes'] ?? '-', 'class' => 'col-medals medal--bronze'],
+				['data' => $m['InternationalParticipants'] ?? '-', 'class' => 'col-medals'],
+				['data' => $m['RegionalGolds'] ?? '-', 'class' => 'col-medals medal--gold'],
+				['data' => $m['RegionalSilvers'] ?? '-', 'class' => 'col-medals medal--silver'],
+				['data' => $m['RegionalBronzes'] ?? '-', 'class' => 'col-medals medal--bronze'],
+				['data' => $m['RegionalParticipants'] ?? '-', 'class' => 'col-medals'],
+				['data' => $m['NationalGolds'] ?? '-', 'class' => 'col-medals medal--gold'],
+				['data' => $m['NationalSilvers'] ?? '-', 'class' => 'col-medals medal--silver'],
+				['data' => $m['NationalBronzes'] ?? '-', 'class' => 'col-medals medal--bronze'],
+				['data' => $m['NationalParticipants'] ?? '-', 'class' => 'col-medals'],
 			);
 		}
 
@@ -255,7 +231,7 @@ class Statistics extends BaseController {
 			order by t.Alias asc
 		QUERY, [$id])->getResultArray();
 
-		$medals = $this->getMedalStatistics($id);
+		$medals = $this->getPersonMedals($id);
 
 		$table = new \CodeIgniter\View\Table();
 		$table->setTemplate([
@@ -263,9 +239,9 @@ class Statistics extends BaseController {
 		]);
 
 		$heading = array(
-			['data' => 'Internasional', 'class' => 'col-centered', 'colspan' => '3'],
-			['data' => 'Regional', 'class' => 'col-centered', 'colspan' => '3'],
-			['data' => 'Nasional', 'class' => 'col-centered', 'colspan' => '3']
+			['data' => 'Internasional', 'class' => 'col-centered', 'colspan' => '4'],
+			['data' => 'Regional', 'class' => 'col-centered', 'colspan' => '4'],
+			['data' => 'Nasional', 'class' => 'col-centered', 'colspan' => '4']
 		);
 		$table->setHeading($heading);
 
@@ -273,12 +249,15 @@ class Statistics extends BaseController {
 			['data' => $medals[0]['InternationalGolds'] ?? '-', 'class' => 'col-statistics-person-medal medal--gold'],
 			['data' => $medals[0]['InternationalSilvers'] ?? '-', 'class' => 'col-statistics-person-medal medal--silver'],
 			['data' => $medals[0]['InternationalBronzes'] ?? '-', 'class' => 'col-statistics-person-medal medal--bronze'],
+			['data' => $medals[0]['InternationalParticipants'] ?? '-', 'class' => 'col-statistics-person-medal'],
 			['data' => $medals[0]['RegionalGolds'] ?? '-', 'class' => 'col-statistics-person-medal medal--gold'],
 			['data' => $medals[0]['RegionalSilvers'] ?? '-', 'class' => 'col-statistics-person-medal medal--silver'],
 			['data' => $medals[0]['RegionalBronzes'] ?? '-', 'class' => 'col-statistics-person-medal medal--bronze'],
+			['data' => $medals[0]['RegionalParticipants'] ?? '-', 'class' => 'col-statistics-person-medal'],
 			['data' => $medals[0]['NationalGolds'] ?? '-', 'class' => 'col-statistics-person-medal medal--gold'],
 			['data' => $medals[0]['NationalSilvers'] ?? '-', 'class' => 'col-statistics-person-medal medal--silver'],
 			['data' => $medals[0]['NationalBronzes'] ?? '-', 'class' => 'col-statistics-person-medal medal--bronze'],
+			['data' => $medals[0]['NationalParticipants'] ?? '-', 'class' => 'col-statistics-person-medal']
 		);
 
 		return view('statistics_person', [
@@ -290,79 +269,6 @@ class Statistics extends BaseController {
 			'regionalTable' => $this->getExternalStatistics('Regional', $contestants, $submissions),
 			'nationalTable' => $this->getNationalStatistics($contestants, $submissions)
 		]);
-	}
-
-	private function getMedalStatistics($id) {
-		return $this->db->query(sprintf(<<<QUERY
-			select c.ID as ID, Name, InternationalGolds, InternationalSilvers, InternationalBronzes, RegionalGolds, RegionalSilvers, RegionalBronzes, NationalGolds, NationalSilvers, NationalBronzes from (
-				select ID, Name from Person %s
-			) as c
-			left join (
-				select Person, count(Medal) as InternationalGolds
-				from Contestant c
-				join Competition comp on comp.ID = c.Competition
-				where comp.Level = 'International' and Medal = 'G'
-				group by Person
-			) as iGolds on c.ID = iGolds.Person
-			left join (
-				select Person, count(Medal) as InternationalSilvers
-				from Contestant c
-				join Competition comp on comp.ID = c.Competition
-				where comp.Level = 'International' and Medal = 'S'
-				group by Person
-			) as iSilvers on c.ID = iSilvers.Person
-			left join (
-				select Person, count(Medal) as InternationalBronzes
-				from Contestant c
-				join Competition comp on comp.ID = c.Competition
-				where comp.Level = 'International' and Medal = 'B'
-				group by Person
-			) as iBronzes on c.ID = iBronzes.Person
-			left join (
-				select Person, count(Medal) as RegionalGolds
-				from Contestant c
-				join Competition comp on comp.ID = c.Competition
-				where comp.Level = 'Regional' and Medal = 'G'
-				group by Person
-			) as rGolds on c.ID = rGolds.Person
-			left join (
-				select Person, count(Medal) as RegionalSilvers
-				from Contestant c
-				join Competition comp on comp.ID = c.Competition
-				where comp.Level = 'Regional' and Medal = 'S'
-				group by Person
-			) as rSilvers on c.ID = rSilvers.Person
-			left join (
-				select Person, count(Medal) as RegionalBronzes
-				from Contestant c
-				join Competition comp on comp.ID = c.Competition
-				where comp.Level = 'Regional' and Medal = 'B'
-				group by Person
-			) as rBronzes on c.ID = rBronzes.Person
-			left join (
-				select Person, count(Medal) as NationalGolds
-				from Contestant c
-				join Competition comp on comp.ID = c.Competition
-				where comp.Level = 'National' and Medal = 'G'
-				group by Person
-			) as nGolds on c.ID = nGolds.Person
-			left join (
-				select Person, count(Medal) as NationalSilvers
-				from Contestant c
-				join Competition comp on comp.ID = c.Competition
-				where comp.Level = 'National' and Medal = 'S'
-				group by Person
-			) as nSilvers on c.ID = nSilvers.Person
-			left join (
-				select Person, count(Medal) as NationalBronzes
-				from Contestant c
-				join Competition comp on comp.ID = c.Competition
-				where comp.Level = 'National' and Medal = 'B'
-				group by Person
-			) as nBronzes on c.ID = nBronzes.Person
-			order by InternationalGolds desc, InternationalSilvers desc, InternationalBronzes desc, RegionalGolds desc, RegionalSilvers desc, RegionalBronzes desc, NationalGolds desc, NationalSilvers desc, NationalBronzes desc, Name asc
-			limit 100
-		QUERY, $id ? 'where ID = ?' : ''), [$id])->getResultArray();
 	}
 
 	private function getExternalStatistics($level, $contestants, $submissions) {
