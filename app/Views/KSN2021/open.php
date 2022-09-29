7fb2fedb65907ca35b892f19c26854daf578d103
Ashar Fuadi <fushar@gmail.com>
2022-09-29 22:20:06 +0700

    Import KSN 2021 open contest

--- app/Views/KSN2021/open.php
+++ app/Views/KSN2021/open.php
@@ -1,15 +1,12 @@
 <div class="bp3-card">
-    <p>We are pleased to invite everybody to our <strong>TOKI Indonesian NOI Open Contest 2022</strong>!</p>
-    
-    <hr />
-
-    <h3 id="summary">Summary</h3>
+    <h3 id="preface">Preface</h3>
 
     <ul>
-    <li><strong>TOKI Indonesian NOI Open Contest 2022</strong> is an open competition mirroring Indonesian NOI 2022 (<strong>OSN 2022 Bidang Informatika</strong> in Bahasa Indonesia).</li>
-    <li>The problemset is the same as the problemset in the original competition and will be given in both Bahasa Indonesia and English.</li>
-    <li>The contest will be held on <a href="https://tlx.toki.id/contests"><strong>TLX</strong></a> (you may need to register for a TLX account if you do not have one).</li>
-    <li>If you have any questions please contact <a href="mailto:sc-osn-2022@googlegroups.com">sc-osn-2022@googlegroups.com</a>.</li>
+    <li><strong>TOKI KSN Open Contest 2021</strong> is an open competition mirroring the National Science Olympiad in Informatics 2021 in Indonesia (<strong>KSN 2021 Bidang Informatika</strong> in Bahasa Indonesia).</li>
+    <li>KSN Informatika is a national olympiad in informatics for high school students. It is one of the stages required for Indonesian students to represent Indonesia in IOI (International Olympiad in Informatics)</li>
+    <li>The problemset is the same as the problemset in KSN 2021 Bidang Informatika and will be given in both Bahasa Indonesia and English.</li>
+    <li>The contest will be held on <a href="https://tlx.toki.id/contests">TLX (TOKI Online Judge)</a> (you may need to register for a TLX account if you do not have one).</li>
+    <li>If you have any questions please contact <a href="mailto:sc-ksn-2021@googlegroups.com">sc-ksn-2021@googlegroups.com</a>.</li>
     </ul>
 
     <hr />
@@ -18,10 +15,10 @@
 
     <ul>
     <li>There will be <strong>one trial session</strong> and <strong>2 (two) competition days</strong>.</li>
-    <li>The result of the trial session does <strong>not</strong> affect scoring.</li>
+    <li>Result of the trial session does <strong>not</strong> affect scoring.</li>
     <li>There will be <strong>3 (three) problems</strong> on each day to be solved in <strong>5 (five) hours</strong>.</li>
     <li>Each contestant may submit up to <strong>50 submissions</strong> for each problem. This limit may be increased for some problems.</li>
-    <li>The supported programming language is <strong>C++</strong>.</li>
+    <li>The supported programming languages are <strong>C and C++</strong>.</li>
     <li>During the competition, each contestant can only see their own scores.</li>
     </ul>
 
@@ -32,14 +29,14 @@
     <ul>
     <li>The contests will be held at the following time ranges:
     <ul>
-    <li><strong>Day 0</strong>: <a href="https://www.timeanddate.com/worldclock/fixedtime.html?msg=TOKI+Indonesian+NOI+Open+Contest+2022+Day+0&amp;iso=20221003T1030&amp;p1=108&amp;ah=22&amp;am=00">3 October 2022 03:30 - 4 October 2022 01:30 UTC</a> (trial session)</li>
-    <li><strong>Day 1</strong>: <a href="https://www.timeanddate.com/worldclock/fixedtime.html?msg=TOKI+Indonesian+NOI+Open+Contest+2022+Day+1&amp;iso=20221004T1330&amp;p1=108&amp;ah=19&amp;am=00">4 October 2022 06:30 - 5 October 2022 01:30 UTC</a></li>
-    <li><strong>Day 2</strong>: <a href="https://www.timeanddate.com/worldclock/fixedtime.html?msg=TOKI+Indonesian+NOI+Open+Contest+2022+Day+2&amp;iso=20221005T1330&amp;p1=108&amp;ah=19&amp;am=00">5 October 2022 06:30 - 6 October 2022 01:30 UTC</a></li>
+    <li><strong>Day 0</strong>: <a href="https://www.timeanddate.com/worldclock/fixedtime.html?msg=TOKI+KSN+Open+Contest+2021+Day+0&amp;iso=20211107T1600&amp;p1=108&amp;ah=16&amp;am=30">7 November 2021 09:00 - 8 November 2021 01:30 UTC</a> (trial session)</li>
+    <li><strong>Day 1</strong>: <a href="https://www.timeanddate.com/worldclock/fixedtime.html?msg=TOKI+KSN+Open+Contest+2021+Day+1&amp;iso=20211108T1330&amp;p1=108&amp;ah=19&amp;am=00">8 November 2021 06:30 - 9 November 2021 01:30 UTC</a></li>
+    <li><strong>Day 2</strong>: <a href="https://www.timeanddate.com/worldclock/fixedtime.html?msg=TOKI+KSN+Open+Contest+2021+Day+2&amp;iso=20211109T1330&amp;p1=108&amp;ah=19&amp;am=00">9 November 2021 06:30 - 10 November 2021 01:30 UTC</a></li>
     </ul>
     </li>
     <li>Each contestant can select <strong>any 5-hour window</strong> within the time range to do the contest for each competition day.</li>
     <li>All three contests are now available on <a href="https://tlx.toki.id/contests">TLX</a>. Please register in those contests.</li>
-    <li>Each contestant may start the contest any time within the time range, by clicking the "start" button.</li>
+    <li>Each contestant may start the contest any time within the time range, by clicking the “start” button.</li>
     <li>There will be no additional time given if a contestant starts the contest in less than 5 hours before the contest ends.</li>
     <li>The open contest does not support clarifications, but all important announcements from the original contest will be broadcast as well.</li>
     </ul>
@@ -49,7 +46,9 @@
     <h3 id="problems-types">Problems Types</h3>
 
     <ul>
-    <li>There are 3 possible problem types. Not all problem types might be given.</li>
+    <li>
+        <p>There are 3 possible problem types. Some problem type might not be tested.</p>
+    </li>
     <li><strong>Batch</strong> problems:
         <ul>
         <li>The most common problem type.</li>
@@ -60,8 +59,8 @@
     </li>
     <li><strong>Interactive</strong> problems:
         <ul>
-        <li>Contestants write a program that interacts with judge's program: judge's program writes to <strong>stdout</strong> and is given as the <strong>stdin</strong> for contestant's program, then contestant's program writes to <strong>stdout</strong> and is given as the <strong>stdin</strong> for judge's program, and the interaction continues until a given goal is reached.</li>
-        <li>Contestant's program must output the answer within the given time and memory limits.</li>
+        <li>Contestants write a program that interacts with judge’s program: judge’s program writes to <strong>stdout</strong> and is given as the <strong>stdin</strong> for contestant’s program, then contestant’s program writes to <strong>stdout</strong> and is given as the <strong>stdin</strong> for judge’s program, and the interaction continues until a given goal is reached.</li>
+        <li>Contestant’s program must output the answer within the given time and memory limits.</li>
         <li>Example: <a href="https://tlx.toki.id/problems/osn-2019/2B">Detecting Gold, OSN 2019</a></li>
         </ul>
     </li>
@@ -81,14 +80,16 @@
 
     <ul>
     <li>There are two types of scoring: <strong>standard</strong> and <strong>creative</strong>.</li>
-    <li>Creative problems will be stated explicitly in the problem description.</li>
+    <li>
+        <p>Creative problems will be stated explicitly in the problem description.</p>
+    </li>
     <li><strong>Standard</strong> scoring:
         <ul>
         <li>For batch and interactive problems:
             <ul>
             <li>A problem consists of <strong>multiple subtasks</strong> with various points.</li>
             <li>A subtask consists of <strong>multiple test cases</strong> that are grouped into some <strong>test groups</strong>.</li>
-            <li>A submission gets a subtask's points if it solves <strong>all of its test groups</strong>.</li>
+            <li>A submission gets a subtask’s points if it solves <strong>all of its test groups</strong>.</li>
             <li>To solve a test group, a submission has to solve <strong>all of its test cases</strong>.</li>
             <li>A problem may have <strong>open subtasks</strong> where the test cases are given to contestants.</li>
             </ul>
@@ -96,7 +97,7 @@
         <li>For output-only problems:
             <ul>
             <li>A problem consists of <strong>multiple test cases</strong> with various points.</li>
-            <li>A submission gets the test case's points if it produces the correct output.</li>
+            <li>A submission gets the test case’s points if it produces the correct output.</li>
             </ul>
         </li>
         </ul>
@@ -106,7 +107,7 @@
         <li>The scoring formula will vary for each problem and will be explicitly stated in the problem description.</li>
         <li>Example:
             <ul>
-            <li>Batch: <a href="https://tlx.toki.id/problems/osn-2013/0C">Tukar-Tukar, OSN 2013</a>.</li>
+            <li>Batch: <a href="https://tlx.toki.id/problems/osn-2012-2/D">Membaca, OSN 2012</a>.</li>
             <li>Interactive: <a href="https://tlx.toki.id/problems/ksn-2020/2C">Finding Balls, KSN 2020</a>.</li>
             <li>Output-only: <a href="https://tlx.toki.id/problems/ksn-2020/1A">National Defence, KSN 2020</a>.</li>
             </ul>
@@ -124,18 +125,18 @@
         <ul>
         <li>On creative scoring, for each submission, the score of a subtask is the minimum score among all its test cases.</li>
         <li>A subtask final score is the <strong>maximum of its score</strong> among all submissions.</li>
-        <li>Contestant's score for a problem is the sum of all its subtasks final score.</li>
+        <li>Contestant’s score for a problem is the sum of all its subtasks final score.</li>
         </ul>
 
-        <p>For example, if the first submission got 30 points for the first subtask and 0 points for the second subtask, and the second submission got 0 points for the first subtask and 40 points for the second subtask, then the contestant's score for the problem is 70 points.</p>
+        <p>For example, if the first submission got 30 points for the first subtask and 0 points for the second subtask, and the second submission got 0 points for the first subtask and 40 points for the second subtask, then the contestant’s score for the problem is 70 points.</p>
     </li>
     <li>For output-only problems:
         <ul>
         <li>A test case final score is the <strong>maximum of its score</strong> among all submissions.</li>
-        <li>A contestant's score on a problem is the sum of all its test cases final score.</li>
+        <li>A contestant’s score on a problem is the sum of all its test cases final score.</li>
         </ul>
     </li>
-    <li>A contestant's total score is the sum of <strong>all of problems score</strong> on <strong>both competition days</strong>.</li>
+    <li>A contestant’s total score is the sum of <strong>all of problems score</strong> on <strong>both competition days</strong>.</li>
     <li>Contestants will be ranked by the total score (descending).</li>
     <li>Two contestants with <strong>the same</strong> total score will achieve <strong>the same</strong> rank.</li>
     <li>Submission time <strong>will not affect</strong> contestant rank at all.</li>
@@ -158,4 +159,5 @@
         </ul>
     </li>
     </ul>
+
 </div>
