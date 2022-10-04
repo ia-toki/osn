0c7679ffcdc5f2516c7218274dc2ee4ea6e7fb3d
Ashar Fuadi <fushar@gmail.com>
2022-10-04 15:02:40 +0700

    Move frozen notice to only results tab

--- app/Views/competition_results.php
+++ app/Views/competition_results.php
@@ -1,5 +1,13 @@
 <?= $this->extend('competition') ?>
 
 <?= $this->section('subcontent') ?>
+	<?php if (!$isFinished) : ?>
+		<div class="bp3-callout bp3-intent-warning">
+			Ini adalah hasil sementara, yang hanya menampilkan <b>hasil 4 jam pertama</b> setiap harinya.
+			<hr />
+			These are preliminary results, showing only the <b>results from the first 4 hours</b> of each day.
+		</div>
+		<br />
+	<?php endif; ?>
 	<?= $table ?>
 <?= $this->endSection() ?>
