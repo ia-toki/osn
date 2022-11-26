b263b2438bff0805726647cb76f75af77bf8497f
Ashar Fuadi <fushar@gmail.com>
2022-11-26 17:17:23 +0700

    Drop Website column from Competition

--- app/Views/competition_info.php
+++ app/Views/competition_info.php
@@ -7,9 +7,6 @@
 			<tbody>
 				<tr><th>Tempat</th><td><?= $competition['City'] ?><?= $competition['HostName'] ? ', ' . $competition['HostName'] : '' ?></td></tr>
 				<tr><th>Waktu</th><td> <?= date_format(date_create($competition['DateBegin']), 'd M Y') . ' &ndash; ' . date_format(date_create($competition['DateEnd']), 'd M Y') ?></td></tr>
-				<?php if ($competition['Website']) : ?>
-					<tr><th>Situs</th><td><a href="<?= $competition['Website'] ?>"><?= $competition['Website'] ?></a></td></tr>
-				<?php endif ?>
 				<?php if ($competition['Contestants']) : ?>
 					<tr><th>Peserta</th><td><?= $competition['Contestants'] ?></td></tr>
 				<?php endif ?>
