fc0cc237acbde416004ae1cd9006f8ca105dd7ff
Ashar Fuadi <fushar@gmail.com>
2022-11-26 11:35:19 +0700

    Add warning when past info is unavailable

--- app/Views/competition_open_results.php
+++ app/Views/competition_open_results.php
@@ -3,5 +3,9 @@
 <?= $this->section('subcontent') ?>
 	<?php if (file_exists(APPPATH . 'Views/' . $competition['ID'] . '/open_results.php')): ?>
 		<?= $this->include($competition['ID'] . '/open_results'); ?>
+	<?php else: ?>
+		<div class="bp3-callout bp3-intent-danger">
+			<em>Informasi tidak tersedia.</em>
+		</div>
 	<?php endif; ?>
 <?= $this->endSection() ?>
--- app/Views/competition_syllabus.php
+++ app/Views/competition_syllabus.php
@@ -1,7 +1,11 @@
 <?= $this->extend('competition') ?>
 
 <?= $this->section('subcontent') ?>
-    <?php if (file_exists(APPPATH . 'Views/' . $competition['ID'] . '/syllabus.php')): ?>
-        <?= $this->include($competition['ID'] . '/syllabus'); ?>
-    <?php endif; ?>
+	<?php if (file_exists(APPPATH . 'Views/' . $competition['ID'] . '/syllabus.php')): ?>
+		<?= $this->include($competition['ID'] . '/syllabus'); ?>
+	<?php else: ?>
+		<div class="bp3-callout bp3-intent-danger">
+			<em>Informasi tidak tersedia.</em>
+		</div>
+	<?php endif; ?>
 <?= $this->endSection() ?>
