fb34a8bc4cd0b26b24f0b5264bf3fb989d1a1ed3
Ashar Fuadi <fushar@gmail.com>
2023-01-20 19:00:56 +0700

    Add KSN Panduan pdfs

---
+++ app/Views/KSN2020/info.php
@@ -0,0 +1,5 @@
+<h3>Referensi</h3>
+
+<ol>
+	<li><a href="/data/KSN2020Pedoman.pdf">Pedoman KSN 2020</a></li>
+</ol>
---
+++ app/Views/KSN2021/info.php
@@ -0,0 +1,5 @@
+<h3>Referensi</h3>
+
+<ol>
+	<li><a href="/data/KSN2021Panduan.pdf">Panduan KSN 2021</a></li>
+</ol>
