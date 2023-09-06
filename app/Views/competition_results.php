7f5b6cd1d24d623d70b49ad669fdf61edfb08e72
Ashar Fuadi <fushar@gmail.com>
2023-09-06 20:35:52 +0700

    Release OSN2023 results

--- app/Views/competition_results.php
+++ app/Views/competition_results.php
@@ -1,12 +1,6 @@
 <?= $this->extend('competition') ?>
 
 <?= $this->section('subcontent') ?>
-	<?php if ($competition['ID'] == 'OSN2023') : ?>
-		<div class="bp3-callout bp3-intent-warning">
-			<p>Berdasarkan peraturan BPTI, detail hasil setiap bidang hanya boleh dirilis beberapa hari setelah pengumuman resmi. 🙏</p>
-		</div>
-		<br />
-	<?php endif; ?>
 	<?php if (!$isDataComplete) : ?>
 		<div class="bp3-callout bp3-intent-warning">
 			<p><b>Data tidak tersedia / tidak lengkap</b>. Memiliki data? Laporkan pada <b><a href="https://github.com/ia-toki/osn">github.com/ia-toki/osn</a></b>.</p>
