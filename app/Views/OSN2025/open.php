09d7c628c43d3e1d3feb626f71361431036339ec
Ashar Fuadi <fushar@gmail.com>
2025-10-01 20:50:23 +0700

    Add open OSN 2025

---
+++ app/Views/OSN2025/open.php
@@ -0,0 +1,155 @@
+<div class="bp3-card">
+  <p>We are pleased to invite everybody to our <strong>TOKI Indonesian NOI Open Contest 2025</strong>!</p>
+  <p style="color:red">Please notice some rule differences from last year's open contest, marked in red below.</p>
+  <hr />
+
+  <h3 id="summary">Summary</h3>
+
+  <ul>
+    <li><strong>TOKI Indonesian NOI Open Contest 2025</strong> is an open competition mirroring Indonesian NOI 2025 (<strong>OSN 2025 Bidang Informatika</strong> in Bahasa Indonesia).</li>
+    <li>The problemset is the same as the problemset in the original competition and will be given in both Bahasa Indonesia and English.</li>
+    <li>The contest will be held on <a href="https://tlx.toki.id/contests"><strong>TLX</strong></a> (you may need to register for a TLX account if you do not have one).</li>
+    <li>If you have any questions please contact <a href="mailto:sc-osn-2025@googlegroups.com">sc-osn-2025@googlegroups.com</a>.</li>
+  </ul>
+
+  <hr />
+
+  <h3 id="rules-overview">Rules Overview</h3>
+
+  <ul>
+    <li>There will be <strong>2 (two) competition days</strong>.</li>
+    <li>There will be <strong>3 (three) problems</strong> on each day to be solved in <strong>5 (five) hours</strong>.</li>
+    <li>Each contestant may submit up to <strong>50 submissions</strong> for each problem. This limit may be increased for some problems.</li>
+    <li>The supported programming language is <strong>C++</strong>.</li>
+    <li>During the competition, each contestant can only see their own scores.</li>
+  </ul>
+
+  <hr />
+
+  <h3 id="schedule-and-technical-details">Schedule and Technical Details</h3>
+
+  <ul>
+    <li>The contests will be held at the following time ranges:
+      <ul>
+        <li><strong>Day 1</strong>: <a href="https://www.timeanddate.com/worldclock/fixedtime.html?msg=TOKI+Indonesian+NOI+Open+Contest+2025+Day+1&amp;iso=20251008T1400&amp;p1=108&amp;ah=16&amp;am=00">8 October 2025 14:00 - 9 October 2025 06:00 UTC+7</a></li>
+        <li><strong>Day 2</strong>: <a href="https://www.timeanddate.com/worldclock/fixedtime.html?msg=TOKI+Indonesian+NOI+Open+Contest+2025+Day+2&amp;iso=20251009T1400&amp;p1=108&amp;ah=16&amp;am=00">9 October 2025 14:00 - 10 October 2025 06:00 UTC+7</a></li>
+      </ul>
+    </li>
+    <li>Each contestant can select <strong>any 5-hour window</strong> within the time range to do the contest for each competition day.</li>
+    <li><a href="https://tlx.toki.id/contests"><strong>Both contests are available on TLX</strong></a>. Please register in those contests.</li>
+    <li>Each contestant may start the contest any time within the time range, by clicking the "start" button.</li>
+    <li>There will be no additional time given if a contestant starts the contest in less than 5 hours before the contest ends.</li>
+    <li>The open contest does not support clarifications, but all important announcements from the original contest will be broadcast as well.</li>
+  </ul>
+
+  <hr />
+
+  <h3 id="problems-types">Problems Types</h3>
+
+  <ul>
+    <li>There are 3 possible problem types. Not all problem types might be given.</li>
+    <li><strong>Batch</strong> problems:
+      <ul>
+        <li>The most common problem type.</li>
+        <li>Given an input, contestants write a program that reads input from <strong>stdin</strong> and writes output to <strong>stdout</strong>.</li>
+        <li>The program must run within the given time and memory limits.</li>
+        <li>Example: <a href="https://tlx.toki.id/problems/osn-2023/1A">Bogor Defence, OSN 2023</a></li>
+      </ul>
+    </li>
+    <li><strong>Interactive</strong> problems:
+      <ul>
+        <li>Contestants write a program that interacts with judge's program: judge's program writes to <strong>stdout</strong> and is given as the <strong>stdin</strong> for contestant's program, then contestant's program writes to <strong>stdout</strong> and is given as the <strong>stdin</strong> for judge's program, and the interaction continues until a given goal is reached.</li>
+        <li>Contestant's program must output the answer within the given time and memory limits.</li>
+        <li>Example: <a href="https://tlx.toki.id/problems/osn-2023/2C">One-Day Delivery, OSN 2023</a></li>
+      </ul>
+    </li>
+    <li><strong>Output-only</strong> problems:
+      <ul>
+        <li>Contestants will be given all the input test cases.</li>
+        <li>Contestants submit <strong>output</strong> for each input test case.</li>
+        <li>Contestants <strong>do not have to</strong> write a program; the outputs may be solved manually.</li>
+        <li>Example: <a href="https://tlx.toki.id/problems/ksn-2020/1A">National Defence, KSN 2020</a>.</li>
+      </ul>
+    </li>
+  </ul>
+
+  <hr />
+
+  <h3 id="problem-scoring">Problem Scoring</h3>
+
+  <ul>
+    <li>For batch and interactive problems:
+      <ul>
+        <li>A problem consists of <strong>multiple subtasks</strong> with various points.</li>
+        <li>A subtask consists of <strong>multiple test cases</strong>.</li>
+        <li>A submission gets a subtask's points if it solves <strong>all of its test cases</strong>.</li>
+        <li>A problem may have <strong>open subtasks</strong> where the test cases are given to contestants.</li>
+      </ul>
+    </li>
+    <li>For output-only problems:
+      <ul>
+        <li>A problem consists of <strong>multiple test cases</strong> with various points.</li>
+        <li>A submission gets the test case's points if it produces the correct output.</li>
+      </ul>
+    </li>
+    <li>For each problem, the score can also be rewarded based on a given scoring formula.
+      <ul>
+        <li>The scoring formula will vary for each problem and will be explicitly stated in the problem description.</li>
+        <li>Example:
+          <ul>
+            <li>Batch: <a href="https://tlx.toki.id/problems/osn-2024/0A">Duck History, OSN 2024</a>.</li>
+            <li>Interactive: <a href="https://tlx.toki.id/problems/osn-2024/0B">National Treasure, OSN 2024</a>.</li>
+            <li>Output-only: <a href="https://tlx.toki.id/problems/osn-2024/0C">Monochrome Mosaic, OSN 2024</a>.</li>
+          </ul>
+        </li>
+      </ul>
+    </li>
+  </ul>
+
+  <hr />
+
+  <h3 id="contestant-scoring">Contestant Scoring</h3>
+
+  <ul>
+    <li>For batch and interactive problems:
+      <ul>
+        <li>On creative scoring, for each submission, the score of a subtask is the minimum score among all its test cases.</li>
+        <li>A subtask final score is the <strong>maximum of its score</strong> among all submissions.</li>
+        <li>Contestant's score for a problem is the sum of all its subtasks final score.</li>
+      </ul>
+
+      <p>For example, if the first submission got 30 points for the first subtask and 0 points for the second subtask, and the second submission got 0 points for the first subtask and 40 points for the second subtask, then the contestant's score for the problem is 70 points.</p>
+    </li>
+    <li>For output-only problems:
+      <ul>
+        <li>A test case final score is the <strong>maximum of its score</strong> among all submissions.</li>
+        <li>A contestant's score on a problem is the sum of all its test cases final score.</li>
+      </ul>
+    </li>
+    <li>A contestant's total score is the sum of <strong>all of problems score</strong> on <strong>both competition days</strong>.</li>
+    <li>Contestants will be ranked first by the <strong>total score</strong> (descending).</li>
+    <li style="color: red">Then, two contestants with <strong>the same</strong> total score will be ranked by <strong>time penalty</strong> (ascending).
+    <ul><li>Time penalty is defined as the <b>sum</b> of:
+    <ul><li>the time (duration) to reach the final score on Day 1 for the first time</li>
+    <li>the time (duration) to reach the final score on Day 2 for the first time</li>
+    </ul></li></ul></li>
+  </ul>
+
+  <hr />
+
+  <h3 id="grading-system">Grading System</h3>
+
+  <ul>
+    <li><a href="https://tlx.toki.id">TLX</a> will be used as the grading system.</li>
+    <li>Source code limit for each submission is <strong>300 KB</strong>.</li>
+    <li>For each test case, the grader will output one of:
+      <ul>
+        <li><strong>AC</strong> (Accepted): program solved the problem within the given time and memory limits.</li>
+        <li><strong>WA</strong> (Wrong Answer): program stopped within given time and memory limits, but produced the wrong output.</li>
+        <li><strong>RTE</strong> (Runtime Error): program crashed or exceedeed the memory limit.</li>
+        <li><strong>TLE</strong> (Time Limit Exceeded): program exceeded the time limit.</li>
+        <li>Skipped: test case is not graded because there was a test case in the same subtask that was not solved.</li>
+      </ul>
+    </li>
+  </ul>
+</div>
