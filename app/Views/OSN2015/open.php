38ff816196256b41741ac7743b870cce0849daac
Andreas Martin <avmm.martin+github@gmail.com>
2026-03-29 08:20:56 +0700

    feat(OSN2015): open osn data (#25)
    Refs: https://github.com/ia-toki/osn-2015-web
    Refs: https://toki.id/download/osn-informatika-terbuka-2015-hasil-internasional/
    Refs: https://toki.id/download/osn-informatika-terbuka-2015-hasil-sma/
    Refs: https://toki.id/download/osn-informatika-terbuka-2015-hasil-umum/
    Refs: https://toki.id/2015/osn-informatika-terbuka-2015/

---
+++ app/Views/OSN2015/open.php
@@ -0,0 +1,46 @@
+<div class="bp3-card">
+    <h3 class="post-title">
+        <a href="https://toki.id/2015/osn-informatika-terbuka-2015/" target="blank">OSN Informatika Terbuka 2015</a>
+    </h3>
+
+    <img src="/images/openosn2015.png" width="250">
+
+    <p><strong>OSN Informatika Terbuka 2015 </strong>(untuk selanjutnya disebut OSN Terbuka 2015) adalah olimpiade yang berbentuk kontes pemrograman yang diadakan bersamaan dengan OSN Informatika 2015 yang diadakan di Yogyakarta (untuk selanjutnya disebut OSN 2015). Tujuan diadakannya OSN Terbuka adalah untuk memberikan kesempatan kepada peserta yang karena satu dan lain hal tidak diundang ke OSN 2015. OSN Terbuka 2015 dilaksanakan dengan menggunakan prosedur dan aturan yang serupa dengan OSN 2015.</p>
+    <p>OSN Terbuka 2015 dilaksanakan secara online. Kontes dapat diikuti dari tempat masing-masing peserta. OSN Terbuka 2015 dapat diikuti oleh siapa pun yang telah memenuhi prosedur pendaftaran. OSN Terbuka 2015 akan dibagi menjadi tiga kategori, <strong>kategori SMA Nasional</strong> (untuk siswa-siswi Indonesia yang <strong>maksimal saat ini duduk di kelas XI SMA</strong>), <strong>kategori Umum Nasional </strong>(untuk yang <strong>minimal sedang duduk di kelas XII SMA</strong>), dan <strong>kategori Internasional</strong>. Pemisahan SMA dan UMUM bertujuan untuk mengetahui kemampuan siswa/i SMA/ sederajat yang berkompetisi di OSN Terbuka 2015 dan mendapatkan penilaian yang bersih (dapat dibandingkan dengan peserta OSN 2015). Untuk Anda yang sudah melewati bangku kelas XI SMA, bisa mengikuti OSN Terbuka 2015 kategori Umum. Sementara itu, kategori Internasional dikhususkan bagi peserta yang bukan Warga Negara Indonesia. Harap mendaftarkan diri pada kontes yang sesuai dengan status anda saat ini.</p>
+
+    <hr />
+
+    <h3><strong>Prosedur Pendaftaran OSN Terbuka 2015</strong></h3>
+
+    <ol>
+        <li>Jika belum memiliki akun TLX-SSO, daftar di <a href="#">TLX-SSO</a></li>
+        <li>Masuk / <em>login </em>TLX-SSO.</li>
+        <li>Pada menu Contests, klik tombol “View” pada kontes yang ingin diikuti, atau klik tautan berikut:
+            <ol>
+                <li>Untuk SMA: <a href="#">Day 0</a>, <a href="#">Day 1</a>, dan <a href="#">Day 2</a></li>
+                <li>Untuk Umum: <a href="#">Day 0</a>, <a href="#">Day 1</a>, dan <a href="#">Day 2</a></li>
+            </ol>
+        </li>
+        <li>Klik Register [<strong><em>Mohon hanya mendaftarkan diri di satu kategori kontes yang sesuai </em></strong>(<strong>SMA Nasional</strong> atau <strong>Umum Nasional</strong>)].</li>
+        <li>Selamat! Anda sudah menjadi peserta OSN Informatika Terbuka 2015. Kontes dapat dimulai setelah waktu di bawah ini</li>
+    </ol>
+
+    <hr />
+
+    <p>OSN Terbuka 2015 akan dibuka seiring dengan dimulainya OSN 2015 di Yogyakarta, dengan jadwal sebagai berikut:</p>
+    <ul>
+        <li>Day 0 : <strong>19</strong> Mei 2015, pukul 15.30 WIB (UTC +7), daftar sebelum pukul 15.00. Anda bisa mulai kapan saja sebelum 20 Mei 2015 pukul 9.00 WIB (UTC +7). Durasi 2 jam.</li>
+        <li>Day 1 : <strong>20</strong> Mei 2015, pukul 10.00 WIB (UTC +7), daftar sebelum pukul 09.30. Anda bisa mulai kapan saja sebelum 21 Mei 2015 pukul 10.00 WIB (UTC +7). Durasi 5 jam.</li>
+        <li>Day 2 : <strong>21</strong> Mei 2015, pukul 10.00 WIB (UTC +7), daftar sebelum pukul 09.30. Anda bisa mulai kapan saja sebelum 22 Mei 2015 pukul 10.00 WIB (UTC +7). Durasi 5 jam.</li>
+    </ul>
+    <p>Pertanyaan mengenai penyelenggaraan OSN Terbuka 2015 dapat ditulis di <a href="#">Grup Facebook Olimpiade Informatika Indonesia</a> atau dikirim melalui email ke <a href="#">alumni@toki.or.id</a> dengan post diawali / subjek email mengandung tag <strong>[OPEN OSN]</strong>.</p>
+    <p>Demikian pengumuman mengenai OSN Terbuka 2015. Selamat berkompetisi, good luck, have fun!</p>
+
+    <hr />
+
+    <p>Lampiran untuk dibaca:</p>
+    <ul>
+        <li><a href="#">OSN Informatika Terbuka 2015 – Sosialisasi</a></li>
+        <li><a href="#">OSN 2015 – Teknis</a></li>
+    </ul>
+</div>
