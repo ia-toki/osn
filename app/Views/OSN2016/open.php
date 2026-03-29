b7a95262a9fa717244494f197e44bb1940c01b3c
AVM.Martin <avmm.martin+github@gmail.com>
2026-03-29 16:18:11 +0700

    feat(OSN2016): open osn data
    Refs: https://codeforces.com/blog/entry/44892
    Refs: https://docs.google.com/spreadsheets/d/1oMI2R2rpVCm1aPFAIvPsLfo78MucKUxFQ2a3OwxR-n4/edit?gid=1390743876#gid=1390743876

---
+++ app/Views/OSN2016/open.php
@@ -0,0 +1,31 @@
+<div class="bp3-card">
+    <p>Indonesia will hold 2016 National Olympiad in Informatics this week (May 16th — May 18th 2015) in Palembang, Indonesia. It is a competition for Indonesian high school students as one of the preliminary round of Indonesian IOI selection. The contest consists of three separated contests:</p>
+
+    <ul>
+        <li>Day 0 (Practice Contest): Monday, May 16th 14.00 — 17.00 (UTC +7)</li>
+        <li>Day 1: Tuesday, May 17th 08.30 — 13.30 (UTC +7)</li>
+        <li>Day 2: Wednesday, May 18th 08.30 — 13.30 (UTC +7)</li>
+    </ul>
+
+    <p>And this is the second year for Indonesia (last year event : <a href="#">here</a>) to invite everyone around the world to try solving the problems by participating the Open Contest! The Open Contest will be held in <a href="#">https://competition.ia-toki.org/</a>.</p>
+
+    <p>There will be three different contests, one for each day. You may participate them separately (e.g. only participate in Day 1 and Day 2, but not in Day 0), but we expect you to join them all. <b>Unlike last year</b>, they are not virtual contests. The Open Contest will start after an hour from the real contest. Here are the schedule and details for the Open Contest:</p>
+
+    <ul>
+        <li>Day 0 (Practice Contest): <a href="http://www.timeanddate.com/worldclock/fixedtime.html?msg=Day+0&iso=20160516T1530&p1=576&ah=2">Monday, May 16th 15.30 — 24.00 (UTC +7)</a>. Register <a href="#">here.</a></li>
+        <li>Day 1: <a href="http://www.timeanddate.com/worldclock/fixedtime.html?msg=Day+1&iso=20160517T0930&p1=576&ah=5">Tuesday, May 17th 09.30 — 14.30 (UTC +7)</a>. Register <a href="#">here.</a></li>
+        <li>Day 2: <a href="http://www.timeanddate.com/worldclock/fixedtime.html?msg=Day+2&iso=20160518T0930&p1=576&ah=5">Wednesday, May 18th 09.30 — 14.30 (UTC +7)</a>. Register <a href="#">here.</a></li>
+    </ul>
+
+    Several notes:
+
+    <ul>
+        <li>No medals/certificates will be awarded to Open Contest participants.</li>
+        <li>Each contest will have 3 IOI-styled problems.</li>
+        <li>Problem statements will be provided in English and Indonesia.</li>
+        <li>There is a submission limit of 30 for each problem.</li>
+    </ul>
+
+    <p>We hope you will find many interesting problems here. Good luck and thanks for your participation! :D</p>
+    <p>Indonesian National Olympiad in Informatics Committee</p>
+</div>
