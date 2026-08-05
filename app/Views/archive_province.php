8caf9a7dd9da38892a42fa6e86f265b01f555ef2
Ashar Fuadi <fushar@gmail.com>
2026-08-05 08:43:11 +0700

    Add OSN-P 2026 archive

--- app/Views/archive_province.php
+++ app/Views/archive_province.php
@@ -12,6 +12,12 @@
 		</tr>
 	</thead>
 	<tbody>
+    	<tr>
+    		<td>OSN-P 2026</td>
+    		<td><a href="/data/OSNP2026.pdf">PDF</a></td>
+    		<td>-</td>
+    		<td><a href="https://tlx.toki.id/problems/osnp-2026">TLX</a></td>
+    	</tr>
 		<tr>
 			<td>OSN-P 2025</td>
 			<td><a href="/data/OSNP2025.pdf">PDF</a></td>
