ae84df28fae66b6ea83da09c7ef7e3d9d3f21b5a
Ashar Fuadi <fushar@gmail.com>
2026-08-22 21:39:48 +0700

    Merge archive subpages

--- app/Controllers/Archive.php
+++ app/Controllers/Archive.php
@@ -1,24 +1,9 @@
 <?php namespace App\Controllers;
 
 class Archive extends BaseController {
-	public function national() {
-		return view('archive_national', [
+	public function index() {
+		return view('archive', [
 			'menu' =>'archive',
-			'submenu' =>'/',
-		]);
-	}
-
-	public function province() {
-		return view('archive_province', [
-			'menu' =>'archive',
-			'submenu' =>'/provinsi',
-		]);
-	}
-
-	public function city() {
-		return view('archive_city', [
-			'menu' =>'archive',
-			'submenu' =>'/kota',
 		]);
 	}
 }
--- app/Views/archive.php
+++ app/Views/archive.php
@@ -2,12 +2,150 @@
 
 <?= $this->section('content') ?>
 <h2>Arsip Soal</h2>
-<div class="bp3-button-group section">
-	<a role="button" href="/arsip/kota" class="bp3-button <?= $submenu == '/kota' ? 'bp3-active' : '' ?>">OSN-K</a>
-	<a role="button" href="/arsip/provinsi" class="bp3-button <?= $submenu == '/provinsi' ? 'bp3-active' : '' ?>">OSN-P</a>
-	<a role="button" href="/arsip" class="bp3-button <?= $submenu == '/' ? 'bp3-active' : '' ?>">OSN</a>
-</div>
 
-<?= $this->renderSection('subcontent') ?>
+<table class="table table-striped table-bordered">
+	<thead>
+		<tr>
+			<th class="col-archive-year">Tahun</th>
+			<th>OSN-K</th>
+			<th>OSN-P</th>
+			<th>OSN</th>
+		</tr>
+	</thead>
+	<tbody>
+		<tr>
+			<td>2026</td>
+			<td><a href="/data/OSNK2026.pdf">Soal</a></td>
+			<td><a href="/data/OSNP2026.pdf">Soal</a> | <a href="https://tlx.toki.id/problems/osnp-2026">TLX</a></td>
+			<td>-</td>
+		</tr>
+		<tr>
+			<td>2025</td>
+			<td><a href="/data/OSNK2025.pdf">Soal</a></td>
+			<td><a href="/data/OSNP2025.pdf">Soal</a> | <a href="https://tlx.toki.id/problems/osnp-2025">TLX</a></td>
+			<td><a href="https://tlx.toki.id/problems/osn-2025">TLX &nbsp;(+ Pembahasan)</a></td>
+		</tr>
+		<tr>
+			<td>2024</td>
+			<td><a href="/data/OSNK2024.pdf">Soal</a></td>
+			<td><a href="/data/OSNP2024.pdf">Soal</a> | <a href="https://tlx.toki.id/problems/osnp-2024">TLX</a></td>
+			<td><a href="https://tlx.toki.id/problems/osn-2024">TLX &nbsp;(+ Pembahasan)</a></td>
+		</tr>
+		<tr>
+			<td>2023</td>
+			<td><a href="/data/OSNK2023.pdf">Soal</a></td>
+			<td><a href="/data/OSNP2023.pdf">Soal</a> | <a href="https://tlx.toki.id/problems/osnp-2023">TLX</a></td>
+			<td><a href="https://tlx.toki.id/problems/osn-2023">TLX &nbsp;(+ Pembahasan)</a></td>
+		</tr>
+		<tr>
+			<td>2022</td>
+			<td><a href="/data/OSNK2022.pdf">Soal</a></td>
+			<td><a href="/data/OSNP2022.pdf">Soal</a> | <a href="https://tlx.toki.id/problems/osnp-2022">TLX</a></td>
+			<td><a href="https://tlx.toki.id/problems/osn-2022">TLX</a> | <a href="https://docs.google.com/presentation/d/10yjaJYmQqGaAin8Eq70OHQQ5ZUtXNFq4cKihXB9oyb4/">Pembahasan</a></td>
+		</tr>
+		<tr>
+			<td>2021</td>
+			<td><a href="/data/KSNK2021.pdf">Soal</a></td>
+			<td><a href="/data/KSNP2021.pdf">Soal</a> | <a href="https://tlx.toki.id/problems/ksnp-2021">TLX</a></td>
+			<td><a href="https://tlx.toki.id/problems/ksn-2021">TLX</a> | <a href="https://docs.google.com/presentation/d/1ZvDBHjdHDLE2z2smygAP-po1peWPzxFGoYqDzoRDTzg/">Pembahasan</a></td>
+		</tr>
+		<tr>
+			<td>2020</td>
+			<td><a href="/data/KSNK2020.pdf">Soal</a></td>
+			<td><a href="/data/KSNP2020.pdf">Soal</a> | <a href="https://tlx.toki.id/problems/ksnp-2020">TLX</a></td>
+			<td><a href="https://tlx.toki.id/problems/ksn-2020">TLX</a> | <a href="https://docs.google.com/presentation/d/1NrPtZ5zKC443hjPsu0EByZqKIhWy5aPMPN6fEHvGqjc/">Pembahasan</a></td>
+		</tr>
+		<tr>
+			<td>2019</td>
+			<td><a href="/data/OSK2019.pdf">Soal</a></td>
+			<td><a href="/data/OSP2019.pdf">Soal</a> | <a href="https://tlx.toki.id/problems/osp-2019">TLX</a> | <a href="https://docs.google.com/document/d/1p3TncIO-klesAS0TdAWYwLLRCVenVzP4F3Kgf6MU7Jg/">Pembahasan</a></td>
+			<td><a href="/data/OSN2019.pdf">Soal</a> | <a href="https://tlx.toki.id/problems/osn-2019">TLX</a> | <a href="/data/OSN2019Pembahasan.pdf">Pembahasan</a></td>
+		</tr>
+		<tr>
+			<td>2018</td>
+			<td><a href="/data/OSK2018.pdf">Soal</a> | <a href="https://www.kujawab.com/OSKKOM18">Kujawab</a></td>
+			<td><a href="/data/OSP2018.pdf">Soal</a> | <a href="https://www.kujawab.com/OSPKOM18">Kujawab</a></td>
+			<td><a href="/data/OSN2018.pdf">Soal</a> | <a href="https://tlx.toki.id/problems/osn-2018">TLX</a> | <a href="/data/OSN2018Pembahasan.pdf">Pembahasan</a></td>
+		</tr>
+		<tr>
+			<td>2017</td>
+			<td><a href="/data/OSK2017.pdf">Soal</a> | <a href="https://www.kujawab.com/OSKKOM17">Kujawab</a></td>
+			<td><a href="/data/OSP2017.pdf">Soal</a> | <a href="https://www.kujawab.com/OSPKOM17">Kujawab</a></td>
+			<td><a href="/data/OSN2017.pdf">Soal</a> | <a href="https://tlx.toki.id/problems/osn-2017">TLX</a> | <a href="/data/OSN2017Pembahasan.pdf">Pembahasan</a></td>
+		</tr>
+		<tr>
+			<td>2016</td>
+			<td><a href="/data/OSK2016.pdf">Soal</a> | <a href="https://www.kujawab.com/OSKKOM16">Kujawab</a> | <a href="https://tlx.toki.id/problems/osk-2016">TLX</a></td>
+			<td><a href="/data/OSP2016.pdf">Soal</a> | <a href="https://www.kujawab.com/OSPKOM16">Kujawab</a></td>
+			<td><a href="/data/OSN2016.pdf">Soal</a> | <a href="https://tlx.toki.id/problems/osn-2016">TLX</a></td>
+		</tr>
+		<tr>
+			<td>2015</td>
+			<td><a href="/data/OSK2015.pdf">Soal</a> | <a href="https://www.kujawab.com/OSKKOM15">Kujawab</a> | <a href="https://tlx.toki.id/problems/osk-2015">TLX</a></td>
+			<td><a href="/data/OSP2015.pdf">Soal</a> | <a href="https://www.kujawab.com/OSPKOM15">Kujawab</a></td>
+			<td><a href="/data/OSN2015.pdf">Soal</a> | <a href="https://tlx.toki.id/problems/osn-2015">TLX</a> | <a href="/data/OSN2015Pembahasan.pdf">Pembahasan</a></td>
+		</tr>
+		<tr>
+			<td>2014</td>
+			<td><a href="/data/OSK2014.pdf">Soal</a> | <a href="https://www.kujawab.com/OSKKOM14">Kujawab</a> | <a href="https://tlx.toki.id/problems/osk-2014">TLX</a></td>
+			<td><a href="/data/OSP2014.pdf">Soal</a> | <a href="https://www.kujawab.com/OSPKOM14">Kujawab</a></td>
+			<td><a href="/data/OSN2014.pdf">Soal</a> | <a href="https://tlx.toki.id/problems/osn-2014">TLX</a></td>
+		</tr>
+		<tr>
+			<td>2013</td>
+			<td><a href="/data/OSK2013.pdf">Soal</a> | <a href="https://www.kujawab.com/OSKKOM13">Kujawab</a> | <a href="https://tlx.toki.id/problems/osk-2013">TLX</a></td>
+			<td><a href="/data/OSP2013.pdf">Soal</a> | <a href="https://www.kujawab.com/OSPKOM13">Kujawab</a></td>
+			<td><a href="/data/OSN2013.pdf">Soal</a> | <a href="https://tlx.toki.id/problems/osn-2013">TLX</a> | <a href="/data/OSN2013Pembahasan.pdf">Pembahasan</a></td>
+		</tr>
+		<tr>
+			<td>2012</td>
+			<td><a href="/data/OSK2012.pdf">Soal</a> | <a href="https://www.kujawab.com/OSKKOM12">Kujawab</a> | <a href="https://tlx.toki.id/problems/osk-2012">TLX</a></td>
+			<td><a href="/data/OSP2012.pdf">Soal</a> | <a href="https://www.kujawab.com/OSPKOM12">Kujawab</a></td>
+			<td><a href="/data/OSN2012.pdf">Soal</a> | <a href="https://tlx.toki.id/problems/osn-2012">TLX</a></td>
+		</tr>
+		<tr>
+			<td>2011</td>
+			<td><a href="/data/OSK2011.pdf">Soal</a> | <a href="https://www.kujawab.com/OSKKOM11">Kujawab</a> | <a href="https://tlx.toki.id/problems/osk-2011">TLX</a></td>
+			<td><a href="/data/OSP2011.pdf">Soal</a> | <a href="https://www.kujawab.com/OSPKOM11">Kujawab</a></td>
+			<td><a href="/data/OSN2011.pdf">Soal</a> | <a href="https://tlx.toki.id/problems/osn-2011">TLX</a></td>
+		</tr>
+		<tr>
+			<td>2010</td>
+			<td><a href="/data/OSK2010.pdf">Soal</a> | <a href="https://www.kujawab.com/OSKKOM10">Kujawab</a></td>
+			<td><a href="/data/OSP2010.pdf">Soal</a> | <a href="https://www.kujawab.com/OSPKOM10">Kujawab</a></td>
+			<td><a href="/data/OSN2010.pdf">Soal</a> | <a href="https://tlx.toki.id/problems/osn-2010">TLX</a></td>
+		</tr>
+		<tr>
+			<td>2009</td>
+			<td><a href="/data/OSK2009.pdf">Soal</a> | <a href="https://www.kujawab.com/OSKKOM09">Kujawab</a></td>
+			<td><a href="/data/OSP2009.pdf">Soal</a></td>
+			<td><a href="/data/OSN2009.pdf">Soal</a> | <a href="https://tlx.toki.id/problems/osn-2009">TLX</a></td>
+		</tr>
+		<tr>
+			<td>2008</td>
+			<td><a href="/data/OSK2008.pdf">Soal</a> | <a href="https://www.kujawab.com/OSKKOM08">Kujawab</a></td>
+			<td><a href="/data/OSP2008.pdf">Soal</a></td>
+			<td><a href="/data/OSN2008.pdf">Soal</a> | <a href="https://tlx.toki.id/problems/osn-2008">TLX</a></td>
+		</tr>
+		<tr>
+			<td>2007</td>
+			<td><a href="/data/OSK2007.pdf">Soal</a> | <a href="https://www.kujawab.com/OSKKOM07">Kujawab</a></td>
+			<td><a href="/data/OSP2007.pdf">Soal</a></td>
+			<td><a href="/data/OSN2007.pdf">Soal</a> | <a href="https://tlx.toki.id/problems/osn-2007">TLX</a></td>
+		</tr>
+		<tr>
+			<td>2006</td>
+			<td><a href="/data/OSK2006.pdf">Soal</a></td>
+			<td><a href="/data/OSP2006.pdf">Soal</a></td>
+			<td><a href="/data/OSN2006.pdf">Soal</a> | <a href="https://tlx.toki.id/problems/osn-2006">TLX</a></td>
+		</tr>
+		<tr>
+			<td>2005</td>
+			<td>-</td>
+			<td>-</td>
+			<td><a href="/data/OSN2005.pdf">Soal</a> | <a href="https://tlx.toki.id/problems/osn-2005">TLX</a></td>
+		</tr>
+	</tbody>
+</table>
 
 <?= $this->endSection() ?>
